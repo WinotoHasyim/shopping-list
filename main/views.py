48d9eb5fe898dd776ac2627707f94e849f8b1ccc
WinotoHasyim <hasyimwinoto@gmail.com>
2023-09-22 15:41:56 +0700

    Push ulang

--- main/views.py
+++ main/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 # Create your views here.
 
