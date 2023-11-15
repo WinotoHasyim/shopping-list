0c25f73c26c489040a7f034a03d3abf8f93e4136
WinotoHasyim <hasyimwinoto@gmail.com>
2023-11-15 16:37:06 +0700

    Tutorial 8

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax, create_product_flutter
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('xml/', show_xml, name='show_xml'),
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 import datetime
+import json
 from django.shortcuts import render, redirect
-from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponseRedirect, HttpResponse, JsonResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from main.models import Product
@@ -37,6 +38,25 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 @csrf_exempt
 def add_product_ajax(request):
